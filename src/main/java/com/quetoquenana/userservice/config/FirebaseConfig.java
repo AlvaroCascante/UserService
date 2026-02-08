56bba5c3a6f3649f69ebddd34dfe16b3ca0548ef
Alvaro Cascante Retana <alvaro.cascante.r@gmail.com>
2026-02-07 19:23:35 -0600

    Manage FIREBASE_CREDENTIALS as base64

--- src/main/java/com/quetoquenana/userservice/config/FirebaseConfig.java
+++ src/main/java/com/quetoquenana/userservice/config/FirebaseConfig.java
@@ -3,40 +3,39 @@ package com.quetoquenana.userservice.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+
 import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Configuration
+@Slf4j
 public class FirebaseConfig {
 
     @PostConstruct
     public void initialize() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-
-            String credentialsPath = System.getenv("FIREBASE_CREDENTIALS_PATH");
-
-            if (credentialsPath == null || credentialsPath.isBlank()) {
-                throw new IllegalStateException(
-                        "FIREBASE_CREDENTIALS_PATH environment variable not set"
-                );
-            }
-
-            try (InputStream serviceAccount =
-                         new FileInputStream(credentialsPath)) {
-
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                        .build();
-
-                FirebaseApp.initializeApp(options);
-            }
+        // If already initialized, do nothing
+        if (FirebaseApp.getApps() != null && !FirebaseApp.getApps().isEmpty()) {
+            return;
         }
+
+        String base64 = System.getenv("FIREBASE_CREDENTIALS_BASE64");
+
+        if (base64 == null || base64.isBlank()) {
+            throw new IllegalStateException("FIREBASE_CREDENTIALS_BASE64 not set");
+        }
+
+        byte[] decoded = Base64.getDecoder().decode(base64);
+        InputStream serviceAccount = new ByteArrayInputStream(decoded);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 }
-
